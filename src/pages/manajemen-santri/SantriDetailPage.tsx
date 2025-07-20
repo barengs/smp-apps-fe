02062ae7ba7abe23b4ecd1e0bd18a97697bf4ceb
[dyad] <git@dyad.sh>
2025-07-20 16:55:42 +0700

    [dyad] Menyesuaikan ukuran kartu foto - wrote 1 file(s)

--- src/pages/manajemen-santri/SantriDetailPage.tsx
+++ src/pages/manajemen-santri/SantriDetailPage.tsx
@@ -103,7 +103,9 @@ const SantriDetailPage: React.FC = () => {
 
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
           <div className="lg:col-span-1">
-            <SantriPhotoCard photoUrl={santri.photo} name={fullName} />
+            <div className="max-w-xs mx-auto lg:mx-0">
+              <SantriPhotoCard photoUrl={santri.photo} name={fullName} />
+            </div>
           </div>
           <div className="lg:col-span-2">
             <Card className="w-full">
