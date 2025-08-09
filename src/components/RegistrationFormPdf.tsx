974705a9a3ddb34568aca55f8d3ba6f5219690b3
[dyad] <barengstudio@gmail.com>
2025-08-09 13:02:12 +0700

    [dyad] Mengurangi margin atas pada bagian tanda tangan dan heading Data Orang Tua/Wali di PDF - wrote 1 file(s)

--- src/components/RegistrationFormPdf.tsx
+++ src/components/RegistrationFormPdf.tsx
@@ -9,7 +9,7 @@ const KOP_SURAT_IMAGE_URL = "/images/KOP PESANTREN.png";
 const styles = StyleSheet.create({
   page: {
     paddingHorizontal: 40,
-    paddingVertical: 20, // Mengurangi margin atas dari 30 menjadi 20
+    paddingVertical: 20,
     backgroundColor: '#ffffff',
     fontFamily: 'Helvetica',
     fontSize: 10,
@@ -40,7 +40,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderTopColor: '#cccccc',
     paddingTop: 10,
-    marginTop: 5,
+    marginTop: 0, // Mengurangi margin atas dari 5 menjadi 0
   },
   sectionTitle: {
     fontSize: 12,
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
     fontFamily: 'Helvetica',
   },
   signatureContainer: {
-    marginTop: 40, // Mengurangi margin atas dari 60 menjadi 40
+    marginTop: 20, // Mengurangi margin atas dari 40 menjadi 20
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
