f9959d6d5f46aa368f2b3b876a845a1b302e1b69
[dyad] <barengstudio@gmail.com>
2025-08-18 12:03:05 +0700

    [dyad] Memperbaiki error Redux context - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import './globals.css';
 import i18n from './i18n.ts';
 import { I18nextProvider } from 'react-i18next';
 import { LockScreenProvider } from './contexts/LockScreenContext';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <LockScreenProvider>
-        <App />
-      </LockScreenProvider>
-    </I18nextProvider>
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <LockScreenProvider>
+          <App />
+        </LockScreenProvider>
+      </I18nextProvider>
+    </Provider>
   </React.StrictMode>,
 );
