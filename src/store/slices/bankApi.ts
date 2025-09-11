cc95aa4598193ea76041f9eac9400630a3b43b2d
rofiareiv <kambing.rofi@gmail.com>
2025-09-12 05:03:14 +0700

    perubahan dalam endpoint

--- src/store/slices/bankApi.ts
+++ src/store/slices/bankApi.ts
@@ -4,16 +4,16 @@ import { TransaksiApiResponse, SingleTransaksiApiResponse } from '@/types/keuang
 export const bankApi = smpApi.injectEndpoints({
   endpoints: (builder) => ({
     getTransactions: builder.query<TransaksiApiResponse, void>({
-      query: () => 'transaction',
+      query: () => 'main/transaction',
       providesTags: ['Transaksi'],
     }),
     getTransactionById: builder.query<SingleTransaksiApiResponse, string>({
-      query: (id) => `transaction/${id}`,
+      query: (id) => `main/transaction/${id}`,
       providesTags: (result, error, id) => [{ type: 'Transaksi', id }],
     }),
     getTransactionsByAccount: builder.query<TransaksiApiResponse, { accountNumber: string; days?: number }>({
       query: ({ accountNumber, days }) => {
-        let url = `account/${accountNumber}/transactions`;
+        let url = `main/account/${accountNumber}/transactions`;
         if (days) {
           url += `?days=${days}`;
         }
@@ -23,14 +23,14 @@ export const bankApi = smpApi.injectEndpoints({
     }),
     validateTransaction: builder.mutation<{ message: string }, { id: string }>({
       query: ({ id }) => ({
-        url: `transaction/${id}/activate`,
+        url: `main/transaction/${id}/activate`,
         method: 'PUT', // Perbaikan: Mengubah metode dari POST menjadi PUT
       }),
       invalidatesTags: (result, error, { id }) => [{ type: 'Transaksi', id }, 'Transaksi'],
     }),
     addTransaction: builder.mutation<any, { destination_account: string; transaction_type: string; amount: string; description: string }>({
       query: (newTransaction) => ({
-        url: 'transaction',
+        url: 'main/transaction',
         method: 'POST',
         body: newTransaction,
       }),
