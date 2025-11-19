fa64d7ef704781a562893933cf2138dcf8c0d13f
rofiareiv <kambing.rofi@gmail.com>
2025-11-19 15:05:04 +0700

    [dyad] Tambah breadcrumb di Kategori Pelanggaran - wrote 1 file(s)

--- src/pages/manajemen-kamtib/KategoriPelanggaranPage.tsx
+++ src/pages/manajemen-kamtib/KategoriPelanggaranPage.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import DashboardLayout from '@/layouts/DashboardLayout';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import KategoriPelanggaranTable from './KategoriPelanggaranTable';
+import CustomBreadcrumb, { BreadcrumbItemData } from '@/components/CustomBreadcrumb';
 
 const KategoriPelanggaranPage: React.FC = () => {
+  const breadcrumbItems: BreadcrumbItemData[] = [
+    { label: 'Tata Tertib', href: '/dashboard/manajemen-kamtib/pelanggaran' },
+    { label: 'Kategori Pelanggaran' },
+  ];
+
   return (
     <DashboardLayout title="Kategori Pelanggaran" role="administrasi">
+      <CustomBreadcrumb items={breadcrumbItems} />
       <Card>
         <CardHeader>
           <CardTitle>Kategori Pelanggaran</CardTitle>
