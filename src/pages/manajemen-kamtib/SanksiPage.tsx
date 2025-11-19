5139da9187e17d3bec4012f648d74ff4c82e08f1
rofiareiv <kambing.rofi@gmail.com>
2025-11-19 14:31:24 +0700

    [dyad] Perbaiki tampilan Sanksi dengan kartu dan breadcrumb - wrote 1 file(s)

--- src/pages/manajemen-kamtib/SanksiPage.tsx
+++ src/pages/manajemen-kamtib/SanksiPage.tsx
@@ -4,13 +4,28 @@ import React from 'react';
 import DashboardLayout from '@/layouts/DashboardLayout';
 import SanksiTable from './SanksiTable';
 import { useTranslation } from 'react-i18next';
+import CustomBreadcrumb from '@/components/CustomBreadcrumb';
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 
 const SanksiPage: React.FC = () => {
   const { t } = useTranslation();
   return (
     <DashboardLayout title={t('sidebar.sanction')} role="administrasi">
-      <div className="container mx-auto">
-        <SanksiTable />
+      <div className="container mx-auto space-y-4">
+        <CustomBreadcrumb
+          items={[
+            { label: t('sidebar.securityManagement'), href: '/dashboard/manajemen-kamtib/pelanggaran' },
+            { label: t('sidebar.sanction') },
+          ]}
+        />
+        <Card>
+          <CardHeader>
+            <CardTitle>{t('sidebar.sanction')}</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <SanksiTable />
+          </CardContent>
+        </Card>
       </div>
     </DashboardLayout>
   );
