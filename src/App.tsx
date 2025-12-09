3e486908173d2e64ff141f124c4bb08dc5d303ca
rofiareiv <kambing.rofi@gmail.com>
2025-12-09 10:24:40 +0700

    [dyad] Investigasi removeChild error dengan membaca root dan komponen utama - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -86,8 +86,6 @@ import PerizinanPage from '@/pages/manajemen-kamtib/PerizinanPage';
 import TipeIzinPage from '@/pages/manajemen-kamtib/TipeIzinPage';
 
 // Redux imports
-import { Provider } from 'react-redux';
-import { store } from './store';
 import { ThemeProvider } from './components/theme-provider';
 import { ToastContainer } from 'react-toastify';
 import AuthManager from './components/AuthManager';
@@ -112,120 +110,118 @@ function App() {
   }, [settings]);
 
   return (
-    <Provider store={store}>
-      <BrowserRouter
-        future={{
-          v7_startTransition: true,
-        }}
-      >
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <ToastContainer />
-          <AuthManager>
-            <DynamicAppConfig />
-            <Routes>
-              {/* Landing Page Routes */}
-              <Route path="/" element={<Index />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/daftar" element={<Daftar />} />
-              <Route path="/berita/:id" element={<BeritaDetailPage />} />
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+      }}
+    >
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <ToastContainer />
+        <AuthManager>
+          <DynamicAppConfig />
+          <Routes>
+            {/* Landing Page Routes */}
+            <Route path="/" element={<Index />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/daftar" element={<Daftar />} />
+            <Route path="/berita/:id" element={<BeritaDetailPage />} />
 
-              {/* Rute yang Dilindungi */}
-              <Route element={<ProtectedRoute />}>
-                <Route path="/dashboard/wali-santri" element={<WaliSantriDashboard />} />
-                <Route path="/dashboard/administrasi" element={<AdministrasiDashboard />} />
-                <Route path="/dashboard/staf" element={<StafPage />} />
-                <Route path="/dashboard/staf/:id" element={<StaffDetailPage />} />
-                <Route path="/dashboard/staf/:id/edit" element={<StaffEditPage />} />
-                <Route path="/dashboard/hak-akses" element={<HakAksesPage />} />
-                <Route path="/dashboard/peran" element={<PeranPage />} />
-                <Route path="/dashboard/peran/:id" element={<PeranDetailPage />} />
-                <Route path="/dashboard/organisasi" element={<OrganisasiPage />} />
-                
-                {/* Rute Manajemen Santri */}
-                <Route path="/dashboard/santri" element={<ManajemenSantriPage />} />
-                <Route path="/dashboard/santri/:id" element={<SantriDetailPage />} />
-                <Route path="/dashboard/santri/:id/edit" element={<SantriEditPage />} />
-                
-                {/* Rute Pendaftaran Santri */}
-                <Route path="/dashboard/pendaftaran-santri" element={<CalonSantriPage />} />
-                <Route path="/dashboard/pendaftaran-santri/add" element={<SantriFormPage />} />
-                <Route path="/dashboard/pendaftaran-santri/:id/edit" element={<CalonSantriEditPage />} />
-                <Route path="/dashboard/calon-santri/:id" element={<CalonSantriDetailPage />} />
+            {/* Rute yang Dilindungi */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard/wali-santri" element={<WaliSantriDashboard />} />
+              <Route path="/dashboard/administrasi" element={<AdministrasiDashboard />} />
+              <Route path="/dashboard/staf" element={<StafPage />} />
+              <Route path="/dashboard/staf/:id" element={<StaffDetailPage />} />
+              <Route path="/dashboard/staf/:id/edit" element={<StaffEditPage />} />
+              <Route path="/dashboard/hak-akses" element={<HakAksesPage />} />
+              <Route path="/dashboard/peran" element={<PeranPage />} />
+              <Route path="/dashboard/peran/:id" element={<PeranDetailPage />} />
+              <Route path="/dashboard/organisasi" element={<OrganisasiPage />} />
+              
+              {/* Rute Manajemen Santri */}
+              <Route path="/dashboard/santri" element={<ManajemenSantriPage />} />
+              <Route path="/dashboard/santri/:id" element={<SantriDetailPage />} />
+              <Route path="/dashboard/santri/:id/edit" element={<SantriEditPage />} />
+              
+              {/* Rute Pendaftaran Santri */}
+              <Route path="/dashboard/pendaftaran-santri" element={<CalonSantriPage />} />
+              <Route path="/dashboard/pendaftaran-santri/add" element={<SantriFormPage />} />
+              <Route path="/dashboard/pendaftaran-santri/:id/edit" element={<CalonSantriEditPage />} />
+              <Route path="/dashboard/calon-santri/:id" element={<CalonSantriDetailPage />} />
 
-                <Route path="/dashboard/wali-santri-list" element={<WaliSantriListPage />} />
-                <Route path="/dashboard/wali-santri/:id" element={<WaliSantriDetailPage />} />
-                <Route path="/dashboard/wali-santri/:id/edit" element={<WaliSantriEditPage />} />
-                <Route path="/dashboard/informasi-santri" element={<InformasiSantriPage />} />
-                <Route path="/dashboard/nilai-absensi" element={<NilaiAbsensiPage />} />
-                <Route path="/dashboard/bank-santri" element={<BankSantriPage />} />
-                <Route path="/dashboard/bank-santri/transaksi" element={<TransaksiPage />} />
-                <Route path="/dashboard/bank-santri/transaksi/:id" element={<TransaksiDetailPage />} />
-                <Route path="/dashboard/bank-santri/produk" element={<ProdukPage />} />
-                <Route path="/dashboard/bank-santri/coa" element={<CoaPage />} />
-                <Route path="/dashboard/bank-santri/jenis-transaksi" element={<JenisTransaksiPage />} />
-                <Route path="/dashboard/bank-santri/rekening" element={<RekeningPage />} />
-                <Route path="/dashboard/bank-santri/rekening/:accountNumber" element={<RekeningDetailPage />} />
-                <Route path="/dashboard/bank-santri/laporan" element={<LaporanPage />} />
-                <Route path="/dashboard/pengumuman" element={<PengumumanPage />} />
-                <Route path="/dashboard/settings" element={<WaliSantriSettingsPage />} />
-                <Route path="/dashboard/guru-tugas" element={<GuruTugasPage />} />
-                <Route path="/dashboard/penanggung-jawab-magang" element={<PenanggungJawabPage />} />
-                <Route path="/dashboard/institusi-tugas" element={<InstitusiTugasPage />} />
-                <Route path="/dashboard/wilayah/provinsi" element={<ProvinsiPage />} />
-                <Route path="/dashboard/wilayah/kota" element={<KotaPage />} />
-                <Route path="/dashboard/wilayah/kecamatan" element={<KecamatanPage />} />
-                <Route path="/dashboard/wilayah/desa" element={<DesaPage />} />
-                <Route path="/dashboard/pendidikan/program" element={<ProgramPage />} />
-                <Route path="/dashboard/pendidikan/tahun-ajaran" element={<TahunAjaranPage />} />
-                <Route path="/dashboard/pendidikan/asrama" element={<AsramaPage />} />
-                <Route path="/dashboard/pendidikan/jenjang" element={<JenjangPendidikanPage />} />
-                <Route path="/dashboard/pendidikan/kelas" element={<KelasPage />} />
-                <Route path="/dashboard/pendidikan/rombel" element={<RombelPage />} />
-                <Route path="/dashboard/pendidikan/kelompok-pendidikan" element={<KelompokPendidikanPage />} />
-                <Route path="/dashboard/kepesantrenan/kamar" element={<KamarPage />} />
-                <Route path="/dashboard/jadwal" element={<JadwalKegiatanPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/mata-pelajaran" element={<MataPelajaranPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/mata-pelajaran/add" element={<MataPelajaranForm />} />
-                <Route path="/dashboard/manajemen-kurikulum/mata-pelajaran/:id/edit" element={<MataPelajaranForm />} />
-                <Route path="/dashboard/manajemen-kurikulum/jam-pelajaran" element={<JamPelajaranPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/siswa" element={<SiswaPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/siswa/detail" element={<SiswaDetailPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/guru" element={<GuruPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/guru/add" element={<GuruFormPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/guru/:id/edit" element={<GuruFormPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/guru/:id" element={<GuruDetailPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/penugasan-guru" element={<PenugasanGuruPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/jam-mengajar" element={<TeachingHoursPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/presensi" element={<PresensiPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/presensi/:detailId" element={<PresensiDetailPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/presensi/:detailId/pertemuan/:meetingNumber" element={<PresensiFormPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/jadwal-pelajaran" element={<JadwalPelajaranPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/kenaikan-kelas" element={<KenaikanKelasPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/institusi-pendidikan" element={<InstitusiPendidikanPage />} />
-                <Route path="/dashboard/manajemen-kurikulum/institusi-pendidikan/tambah" element={<InstitusiPendidikanForm onSuccess={() => {}} onCancel={() => {}} />} />
-                <Route path="/dashboard/manajemen-kurikulum/institusi-pendidikan/:id" element={<InstitusiPendidikanPage />} />
-                <Route path="/dashboard/manajemen-kamtib/pelanggaran" element={<PelanggaranPage />} />
-                <Route path="/dashboard/manajemen-kamtib/kategori-pelanggaran" element={<KategoriPelanggaranPage />} />
-                <Route path="/dashboard/manajemen-kamtib/sanksi" element={<SanksiPage />} />
-                <Route path="/dashboard/manajemen-kamtib/laporan" element={<LaporanKamtibPage />} />
-                <Route path="/dashboard/manajemen-kamtib/laporan/:id" element={<LaporanDetailPage />} />
-                <Route path="/dashboard/manajemen-kamtib/perizinan" element={<PerizinanPage />} />
-                <Route path="/dashboard/manajemen-kamtib/tipe-izin" element={<TipeIzinPage />} />
-                <Route path="/dashboard/master-data/pekerjaan" element={<PekerjaanPage />} />
-                <Route path="/dashboard/berita" element={<BeritaPage />} />
-                <Route path="/dashboard/settings/navigation" element={<NavigationManagementPage />} />
-                <Route path="/dashboard/settings/app-profile" element={<AppProfilePage />} />
-                <Route path="/dashboard/profile" element={<UserProfilePage />} />
-                <Route path="/dashboard/profile/edit" element={<ProfileEditPage />} />
-              </Route>
+              <Route path="/dashboard/wali-santri-list" element={<WaliSantriListPage />} />
+              <Route path="/dashboard/wali-santri/:id" element={<WaliSantriDetailPage />} />
+              <Route path="/dashboard/wali-santri/:id/edit" element={<WaliSantriEditPage />} />
+              <Route path="/dashboard/informasi-santri" element={<InformasiSantriPage />} />
+              <Route path="/dashboard/nilai-absensi" element={<NilaiAbsensiPage />} />
+              <Route path="/dashboard/bank-santri" element={<BankSantriPage />} />
+              <Route path="/dashboard/bank-santri/transaksi" element={<TransaksiPage />} />
+              <Route path="/dashboard/bank-santri/transaksi/:id" element={<TransaksiDetailPage />} />
+              <Route path="/dashboard/bank-santri/produk" element={<ProdukPage />} />
+              <Route path="/dashboard/bank-santri/coa" element={<CoaPage />} />
+              <Route path="/dashboard/bank-santri/jenis-transaksi" element={<JenisTransaksiPage />} />
+              <Route path="/dashboard/bank-santri/rekening" element={<RekeningPage />} />
+              <Route path="/dashboard/bank-santri/rekening/:accountNumber" element={<RekeningDetailPage />} />
+              <Route path="/dashboard/bank-santri/laporan" element={<LaporanPage />} />
+              <Route path="/dashboard/pengumuman" element={<PengumumanPage />} />
+              <Route path="/dashboard/settings" element={<WaliSantriSettingsPage />} />
+              <Route path="/dashboard/guru-tugas" element={<GuruTugasPage />} />
+              <Route path="/dashboard/penanggung-jawab-magang" element={<PenanggungJawabPage />} />
+              <Route path="/dashboard/institusi-tugas" element={<InstitusiTugasPage />} />
+              <Route path="/dashboard/wilayah/provinsi" element={<ProvinsiPage />} />
+              <Route path="/dashboard/wilayah/kota" element={<KotaPage />} />
+              <Route path="/dashboard/wilayah/kecamatan" element={<KecamatanPage />} />
+              <Route path="/dashboard/wilayah/desa" element={<DesaPage />} />
+              <Route path="/dashboard/pendidikan/program" element={<ProgramPage />} />
+              <Route path="/dashboard/pendidikan/tahun-ajaran" element={<TahunAjaranPage />} />
+              <Route path="/dashboard/pendidikan/asrama" element={<AsramaPage />} />
+              <Route path="/dashboard/pendidikan/jenjang" element={<JenjangPendidikanPage />} />
+              <Route path="/dashboard/pendidikan/kelas" element={<KelasPage />} />
+              <Route path="/dashboard/pendidikan/rombel" element={<RombelPage />} />
+              <Route path="/dashboard/pendidikan/kelompok-pendidikan" element={<KelompokPendidikanPage />} />
+              <Route path="/dashboard/kepesantrenan/kamar" element={<KamarPage />} />
+              <Route path="/dashboard/jadwal" element={<JadwalKegiatanPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/mata-pelajaran" element={<MataPelajaranPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/mata-pelajaran/add" element={<MataPelajaranForm />} />
+              <Route path="/dashboard/manajemen-kurikulum/mata-pelajaran/:id/edit" element={<MataPelajaranForm />} />
+              <Route path="/dashboard/manajemen-kurikulum/jam-pelajaran" element={<JamPelajaranPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/siswa" element={<SiswaPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/siswa/detail" element={<SiswaDetailPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/guru" element={<GuruPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/guru/add" element={<GuruFormPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/guru/:id/edit" element={<GuruFormPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/guru/:id" element={<GuruDetailPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/penugasan-guru" element={<PenugasanGuruPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/jam-mengajar" element={<TeachingHoursPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/presensi" element={<PresensiPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/presensi/:detailId" element={<PresensiDetailPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/presensi/:detailId/pertemuan/:meetingNumber" element={<PresensiFormPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/jadwal-pelajaran" element={<JadwalPelajaranPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/kenaikan-kelas" element={<KenaikanKelasPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/institusi-pendidikan" element={<InstitusiPendidikanPage />} />
+              <Route path="/dashboard/manajemen-kurikulum/institusi-pendidikan/tambah" element={<InstitusiPendidikanForm onSuccess={() => {}} onCancel={() => {}} />} />
+              <Route path="/dashboard/manajemen-kurikulum/institusi-pendidikan/:id" element={<InstitusiPendidikanPage />} />
+              <Route path="/dashboard/manajemen-kamtib/pelanggaran" element={<PelanggaranPage />} />
+              <Route path="/dashboard/manajemen-kamtib/kategori-pelanggaran" element={<KategoriPelanggaranPage />} />
+              <Route path="/dashboard/manajemen-kamtib/sanksi" element={<SanksiPage />} />
+              <Route path="/dashboard/manajemen-kamtib/laporan" element={<LaporanKamtibPage />} />
+              <Route path="/dashboard/manajemen-kamtib/laporan/:id" element={<LaporanDetailPage />} />
+              <Route path="/dashboard/manajemen-kamtib/perizinan" element={<PerizinanPage />} />
+              <Route path="/dashboard/manajemen-kamtib/tipe-izin" element={<TipeIzinPage />} />
+              <Route path="/dashboard/master-data/pekerjaan" element={<PekerjaanPage />} />
+              <Route path="/dashboard/berita" element={<BeritaPage />} />
+              <Route path="/dashboard/settings/navigation" element={<NavigationManagementPage />} />
+              <Route path="/dashboard/settings/app-profile" element={<AppProfilePage />} />
+              <Route path="/dashboard/profile" element={<UserProfilePage />} />
+              <Route path="/dashboard/profile/edit" element={<ProfileEditPage />} />
+            </Route>
 
-              {/* Rute Catch-all untuk halaman yang tidak ditemukan */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </AuthManager>
-        </ThemeProvider>
-      </BrowserRouter>
-    </Provider>
+            {/* Rute Catch-all untuk halaman yang tidak ditemukan */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthManager>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
